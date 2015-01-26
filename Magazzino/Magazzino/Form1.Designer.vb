f6190ac2a6c198bd894b02db13aa0b31ae39a4ff
archistico <info@archistico.com>
2015-01-26 16:00:01 +0100

    Alpha
    Gestione carico e scarico magazzino con lettore codice a barre

---
+++ Magazzino/Magazzino/Form1.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.DGV = New System.Windows.Forms.DataGridView()
+        Me.Farmaco = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Numero = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Carico = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Scarico = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(35, 57)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(435, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(389, 148)
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(81, 20)
+        Me.NumericUpDown1.TabIndex = 5
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 155)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Label1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(35, 260)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(208, 74)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Carica"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(262, 260)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(208, 74)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Scarica"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'DGV
+        '
+        Me.DGV.AllowUserToAddRows = False
+        Me.DGV.AllowUserToDeleteRows = False
+        Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Farmaco, Me.Numero, Me.Carico, Me.Scarico})
+        Me.DGV.Location = New System.Drawing.Point(489, 32)
+        Me.DGV.Name = "DGV"
+        Me.DGV.ReadOnly = True
+        Me.DGV.Size = New System.Drawing.Size(499, 302)
+        Me.DGV.TabIndex = 8
+        '
+        'Farmaco
+        '
+        Me.Farmaco.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.Farmaco.FillWeight = 10.30928!
+        Me.Farmaco.HeaderText = "Farmaco"
+        Me.Farmaco.Name = "Farmaco"
+        Me.Farmaco.ReadOnly = True
+        '
+        'Numero
+        '
+        Me.Numero.FillWeight = 189.6907!
+        Me.Numero.HeaderText = "Numero Progressivo"
+        Me.Numero.Name = "Numero"
+        Me.Numero.ReadOnly = True
+        Me.Numero.Width = 120
+        '
+        'Carico
+        '
+        Me.Carico.HeaderText = "Carico"
+        Me.Carico.Name = "Carico"
+        Me.Carico.ReadOnly = True
+        Me.Carico.Width = 50
+        '
+        'Scarico
+        '
+        Me.Scarico.HeaderText = "Scarico"
+        Me.Scarico.Name = "Scarico"
+        Me.Scarico.ReadOnly = True
+        Me.Scarico.Width = 50
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Codice AIC"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(35, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(138, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Numero progressivo scatola"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(35, 103)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(435, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1000, 346)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DGV)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Magazzino"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents DGV As System.Windows.Forms.DataGridView
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Farmaco As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Numero As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Carico As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Scarico As System.Windows.Forms.DataGridViewTextBoxColumn
+
+End Class
---
+++ Magazzino/Magazzino/Form1.vb
@@ -0,0 +1,64 @@
+﻿Public Class Form1
+
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyBase.KeyPress
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = ""
+        TextBox1.Text = ""
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        'Se lunghezza del testo pari a quello del codice AIC
+        If TextBox1.Text.Length = 9 Then
+            'Cerca farmaco
+            If TextBox1.Text = "025038098" Then
+                Label1.Text = "Sanipirina 500 mg compresse"
+                TextBox2.Focus()
+            ElseIf TextBox1.Text = "034420012" Then
+                Label1.Text = "Lattulosio Ratiopharm  - 100 ml sciroppo"
+                TextBox2.Focus()
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Label1.Text IsNot "" Then
+            DGV.Rows.Add(New String() {Label1.Text, TextBox2.Text, Nothing, NumericUpDown1.Value})
+            MessageBox.Show("Scaricato")
+            Label1.Text = ""
+            TextBox1.Text = ""
+            NumericUpDown1.Value = 1
+            TextBox2.Text = ""
+            TextBox1.Focus()
+        Else
+            MessageBox.Show("Nessun farmaco con questo codice")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Label1.Text IsNot "" Then
+            DGV.Rows.Add(New String() {Label1.Text, TextBox2.Text, NumericUpDown1.Value, Nothing})
+            MessageBox.Show("Caricato")
+            Label1.Text = ""
+            TextBox1.Text = ""
+            NumericUpDown1.Value = 1
+            TextBox2.Text = ""
+            TextBox1.Focus()
+        Else
+            MessageBox.Show("Nessun farmaco con questo codice")
+        End If
+    End Sub
+
+
+    Private Sub HandleEnterAsTab(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles TextBox1.KeyDown, TextBox2.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            Dim CurrentControl As Control = CType(sender, Control)
+            'Me.SelectNextControl(CurrentControl, True, True, True, True)
+            e.SuppressKeyPress = True
+
+        End If
+    End Sub
+End Class
